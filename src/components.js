281e405093d972ae8cdd6ee5b70ed5703e8bf2cd
Tyler Long <tyler4long@gmail.com>
2018-12-12 13:25:40 +0800

    Login and logout

--- src/components.js
+++ src/components.js
@@ -1,12 +1,16 @@
+import React from 'react'
 import { Component } from 'react-subx'
+
+import rc from './ringcentral'
+import config from './config'
 
 export class App extends Component {
   render () {
     const store = this.props.store
-    if (store.token) {
-      return 'logout'
-    } else {
-      return 'login'
+    if (!store.token) {
+      const authorizeUri = rc.authorizeUri(config.OAUTH_REDIRECT_URI, { responseType: 'code' })
+      return <a href={authorizeUri}>Log in</a>
     }
+    return <button onClick={e => rc.token(undefined)}>Log out</button>
   }
 }
--- src/store.js
+++ src/store.js
@@ -2,6 +2,16 @@ import SubX from 'subx'
 import Cookies from 'js-cookie'
 
 import rc from './ringcentral'
+import config from './config'
+
+const setCookie = Cookies.set.bind(Cookies)
+Cookies.set = (key, value, options) => {
+  if (value === undefined) {
+    Cookies.remove(key)
+  } else {
+    setCookie(key, value, options)
+  }
+}
 
 const token = Cookies.getJSON('RINGCENTRAL_TOKEN')
 
@@ -12,8 +22,18 @@ const store = SubX.create({
 
 rc.token(token)
 rc.on('tokenChanged', async token => {
+  const oldToken = store.token
   store.token = token
   Cookies.set('RINGCENTRAL_TOKEN', token, { expires: 7 })
+  if (!oldToken && token) {
+    window.location.replace(config.OAUTH_REDIRECT_URI)
+  }
 })
+
+const urlParams = new URLSearchParams(window.location.search)
+const code = urlParams.get('code')
+if (code) {
+  rc.authorize({ code, redirectUri: config.OAUTH_REDIRECT_URI })
+}
 
 export default store
